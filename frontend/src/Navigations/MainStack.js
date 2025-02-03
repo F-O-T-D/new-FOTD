ba512fda69a9737ebb689b9b021b2884b44f69bc
Yeongeunn <Yeongeunn>
2025-02-03 23:28:47 +0900

    feat: 일기 등록 로직 구현

--- frontend/src/Navigations/MainStack.js
+++ frontend/src/Navigations/MainStack.js
@@ -12,7 +12,6 @@ const Stack = createNativeStackNavigator();
 const MainStack = () => {
   return (
     <Stack.Navigator screenOptions={{ contentStyle: { backgroundColor: WHITE } }}>
-    
       <Stack.Screen name="DiaryListScreen" component={DiaryListScreen} options={{ headerShown: false }} />
       <Stack.Screen name="DiaryEntryScreen" component={DiaryEntryScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
