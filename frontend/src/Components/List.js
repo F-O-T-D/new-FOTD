fc43af9a5623e73a174a69a2f00f195da39534f6
Yeongeunn <Yeongeunn>
2025-02-05 04:10:00 +0900

    fix: 먹킷리스트 UI 개선

--- frontend/src/Components/List.js
+++ frontend/src/Components/List.js
@@ -1,49 +1,48 @@
-import { FlatList, StyleSheet, View, TouchableOpacity } from 'react-native';
-import { GRAY } from '../Colors';
+import React, { useEffect, useRef } from 'react';
+import { FlatList, StyleSheet, Animated, View } from 'react-native';
 import PropTypes from 'prop-types';
 import ListItem from './ListItem';
 import { useNavigation } from '@react-navigation/native';
 import { MapRoutes } from '../Navigations/Routes';
 
-const Separator = () => {
-  return <View style={styles.separator}></View>;
-};
-
 const List = ({ data, onDeleteItem }) => {
-  const navigation = useNavigation(); // useNavigation 훅 사용
+  const navigation = useNavigation();
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, []);
 
   const navigateToMapList = (item) => {
     navigation.navigate(MapRoutes.LISTMAP, { item });
   };
 
   return (
-    <FlatList
-      data={data}
-      //keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => navigateToMapList(item)}>
-          <ListItem item={item} onDeleteItem={onDeleteItem} />
-        </TouchableOpacity>
-      )}
-      windowSize={2}
-      ItemSeparatorComponent={Separator}
-      ListHeaderComponent={View}
-      ListHeaderComponentStyle={{ height: 10 }}
-    />
-
+    <Animated.View style={{ opacity: fadeAnim }}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <ListItem item={item} onDeleteItem={onDeleteItem} onPressItem={() => navigateToMapList(item)} />
+        )}
+        windowSize={2}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
+    </Animated.View>
   );
 };
 
 List.propTypes = {
   data: PropTypes.array.isRequired,
+  onDeleteItem: PropTypes.func.isRequired,
 };
 
 const styles = StyleSheet.create({
   separator: {
-    height: 1,
-    backgroundColor: GRAY.LIGHT,
-    marginVertical: 10,
-    marginHorizontal: 10,
+    height: 12, // ✅ 리스트 아이템 사이의 여백 조정
   },
 });
 
