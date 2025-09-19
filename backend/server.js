676b7a5b10069e6aaa663aaffccfe92b31da9469
Yeongeunn <nye0817@gmail.com>
2025-09-19 15:24:58 +0900

    fix: 회원가입 네트워크 오류 해결 및 관련 코드 리팩토링
    - Android에서 HTTP 통신 오류 해결을 위해 app.json에 usesCleartextTraffic 설정 추가
    - 회원가입 실패 시 더 상세한 오류를 기록하고 Alert에 표시하도록 디버깅 로직 개선
    - insert -> signup으로 회원가입 함수명 변경
    - map 관련 서비스/컨트롤러dptj map -> muckit으로 변경

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,5 @@ console.table(listEndpoints(app));
 // 서버 시작
 app.listen(PORT, "0.0.0.0", () => {  
     console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
-    console.log(`http://localhost:${PORT}`);
 });
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '10.50.98.193', // PC의 IP 주소 사용
+    baseURL: '172.24.97.140', // PC의 IP 주소 사용
   });
   
 export default api;
